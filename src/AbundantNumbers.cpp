cff1dae2d1251be58e19b38c2d6db4c1e50f9f36
Jacec <jacecardon@mail.weber.edu>
2026-01-15 21:23:40 -0700

    Week 2 try it out

--- src/AbundantNumbers.cpp
+++ src/AbundantNumbers.cpp
@@ -7,7 +7,7 @@
 
 using namespace std;
 
-string abundantNum(){
+string abundantNum() {
     //For testing purposes. Please leave in the program
     string testResult;
     stringstream ss_input;
@@ -16,26 +16,40 @@ string abundantNum(){
     int numInput = 0;
     int numSum = 0;
     string result;
-    
+
     //TODO: Create a loop that will execute the program until the user inputs -1
+    while (true) {
         cout << "Enter a number (-1 to quit): ";
+
         //TODO: Prompt the user to input a number. Use the numInput variable.
+        cin >> numInput;
 
-        //TODO: Create the main logic that will calculate if a number is abundant
-        //      Start by creating a for loop that will iterate from 1 to the number
-        //      that the user input.
+        if (numInput == -1) {
+            break;
+        }
 
-        //TODO: If numInput has a remainder then i is NOT a divisor
-        //      If numInput does NOT have a remainder then add it to numSum
+        numSum = 0;
 
-        //TODO: compare numSum to numInput. Using an if-else statement the result variable
-        //      should say one of the following statements:
-        //      result = "a deficient number"
-        //      result = "a perfect number"
-        //      result = "an abundant number"
-        //TODO: Finally, remember to set numSum back to 0
+        for (int i = 1; i < numInput; i++) {
+            if (numInput % i == 0) {
+                numSum += i;
+            }
+        }
+        if (numSum < numInput) {
+            result = "a deficient number";
+        }
+        else if (numSum == numInput) {
+            result = "a perfect number";
+        }
+        else {
+            result = "an abundant number";
+        }
+        cout << numInput << " is " << result << endl;
 
-        cout << numInput << " is " << result << endl; 
+
+    }
+
+
         //For testing purposes. Please leave in the program
         ss_input << numInput;
         ss_result << result;
@@ -44,10 +58,9 @@ string abundantNum(){
         ss_input.clear(); // Clear any error flags
         ss_result.str("");
         ss_result.clear();
-     
+
     return testResult;
 }
-
 #ifndef TESTING
 int main(){
     abundantNum();
